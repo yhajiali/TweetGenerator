df1f7c20b45f179406cd01085f6e5bdd401d7bff
Yusuf Haji-Ali <95085126+yhajiali@users.noreply.github.com>
2024-02-19 13:12:24 +0000

    + GPT api from OpenAI

---
+++ app/api/gpt/route.ts
@@ -0,0 +1,37 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// Create an OpenAI API client
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request, res: Response) {
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json();
+  console.log("messages:", messages);
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `You are a copywritter who is an expert at writing engaging statements.
+You always start with a strong hook to capture attention. Your posts are less than 280 characters long,
+and they are written in short concise and catchy sentences. You NEVER write hashtags or emojis.`,
+      },
+      ...messages,
+    ],
+    stream: true,
+    temperature: 0.5,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
