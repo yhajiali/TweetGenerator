012d56a5b9af6cc57872287e2ef6f3f471ffda18
Yusuf Haji-Ali <95085126+yhajiali@users.noreply.github.com>
2024-02-19 13:16:23 +0000

    + DALL-E-2 API to generate image for tweet

---
+++ app/dall-e/route.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+
+// Create an OpenAI API client
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request, res: Response) {
+  if (req.method !== "POST") {
+    return new Response("Method not allowed", {
+      status: 405,
+    });
+  }
+
+  // Extract the `prompt` from the body of the request
+  const body = await req.text();
+  const { prompt } = JSON.parse(body);
+
+  try {
+    // Ask Dall-E to generate an image based on the prompt
+    const response = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    const imageUrl = response?.data?.[0]?.url;
+    console.log("imageUrl:", imageUrl);
+    return NextResponse.json({ imageUrl });
+  } catch (error: any) {
+    console.error(error);
+    return new Response(error?.message || error?.toString(), {
+      status: 500,
+    });
+  }
+}
